7d3db2c5a3c7c0123760a0290eb144485d40629d
hgftoyrdcvb <for.eros.use@gmail.com>
2022-06-15 01:05:14 +0900

    trying to fix database connection

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -6,14 +6,17 @@ export default function handler(req, res) {
   async function getResult() {
     const mysql = require('mysql2/promise');
     try {
+      console.log('Trying to connect to database');
+      console.log(process.env.DATABASE_URL);
       const connection = await mysql.createConnection(process.env.DATABASE_URL);
       const [rows] = await connection.query('SELECT * FROM portfolio_career');
+      connection.end();
       res.status(200).json({ result: rows });
     } catch (error) {
+      console.log('Error Occured');
       console.log(error);
-      res.status(200).json({ result: [{ organization: 'Error' }] })
+      res.status(200).json({ result: [{ organization: 'Error' }] });
     }
-    connection.end();
   }
 
   getResult();
