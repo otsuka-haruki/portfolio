7a800a4d2d4d6b932b22def7534fe14ea80f1e71
eros <for.eros.use@gmail.com>
2022-09-07 03:29:22 -0700

    error handling

--- src/pages/api/pageview/increment.js
+++ src/pages/api/pageview/increment.js
@@ -4,12 +4,17 @@ export default async function handler(req, res) {
     }
 
     const { mobile, platform, referrer = null } = JSON.parse(req.body)
+    console.log({ mobile, platform })
     require('dotenv').config()
     const mysql = require('mysql2/promise')
     try {
+        console.log('trying to connect...');
+        console.log(process.env.DATABASE_URL);
         const connection = await mysql.createConnection(process.env.DATABASE_URL)
+        console.log('at least connected');
         // TODO: for now, putting null as referrer
         // TODO: use prisma in the first place
+        console.log('trying to execute sql query...');
         const [rows] = await connection.execute(`
         INSERT INTO pageviews_playground VALUES (null, ${mobile}, '${platform}', ${null});
         `)
